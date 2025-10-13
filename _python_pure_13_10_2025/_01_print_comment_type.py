b700f504de1d98e854389c637deacecd8dae764b
hamitmizrak <hamitmizrak@gmail.com>
2025-10-13 13:48:48 +0300

    comment, data

---
+++ _python_pure_13_10_2025/_01_print_comment_type.py
@@ -0,0 +1,21 @@
+# Single Comment
+
+"""
+Multiple Comment (Docstring)
+"""
+
+
+#################################################################################
+####### print  ##################################################################
+
+# string
+print("Python öğreniyorum")
+
+# Tam sayı
+print(1234)
+
+# Virgüllü sayı
+print(14.53)
+
+# string'ten birden fazla değer yazdırmak istiyorsam
+print("Python","Java","Js","Nodejs")
