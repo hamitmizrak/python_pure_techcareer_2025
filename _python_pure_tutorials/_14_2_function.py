6252a53de521a7545ce93e3135d6f51866caf555
hamitmizrak <hamitmizrak@gmail.com>
2025-10-16 11:48:39 +0300

    Function ==> @beartype

--- _python_pure_tutorials/_14_2_function.py
+++ _python_pure_tutorials/_14_2_function.py
@@ -1,6 +1,44 @@
 ################################################################################
 #### Function   ################################################################
 
+# parametre  default değer
+def hesap_topla5(sayi1,sayi2=20):
+    """docstring
+    Bu function Parametreli Returlu toplama yapar
+    """
+    sayi3=sayi1+sayi2
+    return sayi3
+
+# Fuction calling
+# result5=hesap_topla5(10)
+# print(f"{result5}")
+#
+# result5=hesap_topla5(10,40)
+# print(f"{result5}")
+
+################################################################################
+#### Function beartype #########################################################
+# Tür ihlali -> TypeError
+from beartype import beartype
+
+# 1.YOL
+# python -m pip install beartype  ==> Windows (Uzun Hali)
+# py -m pip install beartype      ==> Windows (Kısaltma)
+# pip3  install beartype          ==> Mac || Linux
+
+# 2.YOL
+# cat requirements.txt
+# pip install -r requirements.txt
+
+@beartype
+def hesap_topla6(sayi1:int=0,sayi2:int=0) -> int:
+    return sayi1+sayi2
+
+print(f"{hesap_topla6()}")
+print(f"{hesap_topla6(3)}")
+print(f"{hesap_topla6(3,6)}")
+# print(f"{hesap_topla6(3,"6")}")
+
 
 
 ################################################################################
